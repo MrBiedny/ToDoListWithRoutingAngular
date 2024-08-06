400c1aa45ed4b0a5f77136243f26c81b431ac54d
MrBiedny <MrBiedny@gmail.com>
2024-08-06 18:38:09 +0200

    initial commit

---
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+import {
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterLink,
+  RouterOutlet,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+})
+export class UserTasksComponent {
+  // userId = input.required<string>();
+  userName = input.required<string>();
+  message = input.required<string>();
+  private usersService = inject(UsersService);
+
+  // userName = computed(
+  //   () => this.usersService.users.find((u) => u.id === this.userId())?.name
+  // );
+}
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName =
+    usersService.users.find(
+      (u) => u.id === activatedRoute.paramMap.get('userId')
+    )?.name || '';
+  return userName;
+};
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute,
+  routerState
+) => {
+  return resolveUserName(activatedRoute, routerState) + "'s Tasks";
+};
